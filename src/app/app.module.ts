b5c94755e4acdefd8397125281e0ff65897ffb9d
IonutRedox <ionut.mcinjau@gmail.com>
2020-05-08 22:11:50 +0300

    Installed NgRx and defined app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from '@app/app.component';
+import { MainPageComponent } from '@app/main-page/main-page.component';
 
 import { AuthenticationModule } from '@app/authentication/authentication.module';
 import { AppRoutingModule } from '@app/app-routing.module';
 import { SharedModule } from '@app/shared/shared.module';
 import { CoreModule } from '@app/core/core.module';
-
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
+import { CustomSerializer } from './shared/utils/custom-route-serializer';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainPageComponent,
   ],
   imports: [
     BrowserModule,
     CoreModule,
     SharedModule,
     AuthenticationModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({ maxAge: 25 }),
+    StoreRouterConnectingModule.forRoot({ serializer: CustomSerializer })
   ],
   providers: [],
   bootstrap: [AppComponent]
